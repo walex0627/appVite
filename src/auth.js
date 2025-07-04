9e9fcb4aa92e28a8bcb1d17707dcb558da0dc9ea
walex0627 <walteralex0627@gmail.com>
2025-07-03 22:25:40 -0500

    libreria para hashear el token

--- src/auth.js
+++ src/auth.js
@@ -3,7 +3,7 @@ export const auth = {
     login: (token, usuario) => {
         localStorage.setItem("token", token)
         localStorage.setItem("user", JSON.stringify(usuario))
-    }, //hashear o encriptar el token
+    }, //hashear o encriptar el token la forma de autenticar el token es jwt, es una libreria
     logout: () => {
         localStorage.removeItem("token");
         localStorage.removeItem("user")
